24024da540096f937d0cd1421f29d127f3b58ddc
hieptq2 <hiep.tq@citigo.com.vn>
2019-05-06 18:16:06 +0700

    update

---
+++ includes/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title><?php echo $title; ?></title>
+
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+  <meta name="description" content="<?php if (isset($meta)) {echo $meta;}?>">
+  <meta name="author" content="Hiep Ta">
+
+  <!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+  <meta property="og:url" content=""/>
+  <meta property="og:title" content=""/>
+  <meta property="og:description" content=""/>
+  <meta property="og:image" content=""/>
+
+  <!-- Twitter -->
+	<meta property="twitter:card" content="">
+	<meta property="twitter:url" content="">
+	<meta property="twitter:title" content="">
+	<meta property="twitter:description" content="">
+	<meta property="twitter:image" content="">
+
+	<!-- Favicon -->
+	<link rel="icon" href="favicon.ico">
+
+	<!-- Styles -->
+	<link rel="stylesheet" href="assets/css/main.css">
+
+	<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <![endif]-->
+
+	<!-- Analytics -->
+	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129171522-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-129171522-1');
+	</script> -->
+
+</head>
+<body>
+<div id="siteContainer" class="l-container">
+	<header id="siteHeader" class="l-header l-header--fixtop l-header--home">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-xl-10">
+					<div class="l-header__wrap">
+						<div class="l-header__menu">
+							<nav>
+								<ul class="list-items">
+									<li class="single-item">
+										<a href="#" class="single-item__link">
+											<h3 class="single-item__title">About</h3>
+											<h6 class="single-item__subtitle"><span>Who I Am</span></h6>
+										</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+						<div class="l-header__logo">
+							<h3 class="header-logo"><a href="#"></a></h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="preloader"></div>
+	</header>
